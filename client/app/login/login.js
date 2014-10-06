0551f89fb36d21e66694d5480a98b4417c2bee13
tkulish <thomas.kulish@gmail.com>
2014-10-05 21:47:59 -0400

    Updated the login.js so after a login if successful moves you to the / directory. Updated the index.html so it starts to show username (not successful).

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -5,11 +5,12 @@ angular.module('plutusApp.login', [
 ])
 
 // This is being used though!
-angular.module('plutusApp').controller('LoginCtrl', function ($scope, $http, plutusNotifier, plutusAuth) {
+angular.module('plutusApp').controller('LoginCtrl', function ($scope, $location, $http, plutusNotifier, plutusAuth) {
     $scope.signin = function(username, password) {
         plutusAuth.authenticateUser(username, password).then(function (success) {
             if(success) {
                 plutusNotifier.notify('success','You have succesfully signed in!');
+                $location.path('/');
             } else {
                 plutusNotifier.notify('error','Username/Password combination incorrect');
             }
